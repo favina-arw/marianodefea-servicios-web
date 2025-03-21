6988eaeb5ac4eb4ae913b3af3c439f2bb28c3e87
federico_v <favina.arw@gmail.com>
2025-03-21 17:57:20 -0300

    proyecto base

---
+++ src/main/java/com/marianodefea/servicios_web/ServiciosWebApplication.java
@@ -0,0 +1,13 @@
+package com.marianodefea.servicios_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciosWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciosWebApplication.class, args);
+	}
+
+}
