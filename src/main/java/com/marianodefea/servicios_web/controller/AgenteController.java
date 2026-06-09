00a14c6e8a471c0c30f600ab1d234f545123cb11
Fede_Ubuntu_DGCT <favina.arw@gmail.com>
2026-06-09 15:56:58 -0300

    Se modifica la asignación de cargos

--- src/main/java/com/marianodefea/servicios_web/controller/AgenteController.java
+++ src/main/java/com/marianodefea/servicios_web/controller/AgenteController.java
@@ -1,12 +1,14 @@
 package com.marianodefea.servicios_web.controller;
 
-import com.marianodefea.servicios_web.dto.ActualizarAgenteDTO;
-import com.marianodefea.servicios_web.dto.AgenteDTO;
-import com.marianodefea.servicios_web.dto.CargoAsignadoDTO;
-import com.marianodefea.servicios_web.dto.ListarAgenteDTO;
+import com.marianodefea.servicios_web.dto.*;
+import com.marianodefea.servicios_web.dto.mapper.AgenteCargoMapper;
 import com.marianodefea.servicios_web.dto.mapper.AgenteMapper;
 import com.marianodefea.servicios_web.model.Agente;
+import com.marianodefea.servicios_web.model.AgenteCargo;
+import com.marianodefea.servicios_web.service.AgenteCargoService;
 import com.marianodefea.servicios_web.service.AgenteService;
+import com.marianodefea.servicios_web.service.CargoService;
+import com.marianodefea.servicios_web.utils.Horario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
@@ -29,6 +31,15 @@ public class AgenteController {
 
     @Autowired
     private AgenteMapper agenteMapper;
+
+    @Autowired
+    private AgenteCargoService agenteCargoService;
+
+    @Autowired
+    private AgenteCargoMapper agenteCargoMapper;
+
+    @Autowired
+    private CargoService cargoService;
 
     @GetMapping("/")
     public String listarAgentes(Model model){
@@ -133,6 +144,107 @@ public class AgenteController {
             redirectAttributes.addFlashAttribute("error", "Error al intentar guardar los cambios.");
         }
         return "redirect:/agentes/ver/" + dto.getId();
+    }
+
+    @GetMapping("/asignar_cargo")
+    public String formAsignar(Model model) {
+        // Le pasamos el DTO vacío a Thymeleaf para que mapee el formulario
+        model.addAttribute("asignacionDTO", new AsignacionCargoDTO());
+
+        // Llenamos los desplegables usando los Services, respetando la arquitectura
+        model.addAttribute("agentes", agenteService.obtenerTodosLosAgentesDTO());
+        model.addAttribute("cargos", cargoService.obtenerActivos());
+        return "asignaciones/crear_asignacion";
+    }
+
+    @GetMapping("/{id}/asignar_cargo")
+    public String formAsignar(@PathVariable Long id, Model model, RedirectAttributes redirectAttributes) {
+        try {
+            Optional<Agente> agente = agenteService.findById(id);
+            if (agente.isPresent()) {
+                model.addAttribute("agente", agente.get());
+                model.addAttribute("cargos", cargoService.obtenerActivos());
+                model.addAttribute("asignacionDTO", new AsignacionCargoDTO());
+            } else {
+                // Si el Optional está vacío, redirigimos acá (no entra al catch)
+                redirectAttributes.addFlashAttribute("error", "El agente que intenta modificar no se encuentra.");
+                return "redirect:/agentes/";
+            }
+        }catch (IllegalArgumentException e) {
+            redirectAttributes.addFlashAttribute("error", "El agente qie intenta modificar, no se encuentra");
+            redirectAttributes.addFlashAttribute("errorExt", "" + e.getMessage());
+            return "redirect:/agentes/";
+        }catch (Exception e){
+            redirectAttributes.addFlashAttribute("error", "Error al intentar asignar el agente");
+            redirectAttributes.addFlashAttribute("errorExt", "" + e.getMessage());
+            return "redirect:/agentes/";
+        }
+        return "asignaciones/crear_asignacion_bis";
+    }
+
+
+    @PostMapping("/guardar_asignacion")
+    public String guardarAsignacion(@ModelAttribute("asignacionDTO") AsignacionCargoDTO dto, RedirectAttributes redirectAttributes) {
+        try {
+            // Le pasamos todo el paquete (IDs, fechas, horas) al Service de una
+            agenteCargoService.asignarCargo(dto);
+            redirectAttributes.addFlashAttribute("success", "¡Cargo asignado al agente con éxito!");
+            return "redirect:/agentes/";
+        }catch(IllegalArgumentException iae) {
+            redirectAttributes.addFlashAttribute("error", "Error al procesar la asignación");
+            redirectAttributes.addFlashAttribute("errorExt", iae.getMessage());
+            return "redirect:/admin/asignaciones/crear";
+        }catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "Error al procesar la asignación: " + e.getMessage());
+            redirectAttributes.addFlashAttribute("errorExt", "" + e.getMessage());
+            return "redirect:/agentes/";
+        }
+
+    }
+
+    @GetMapping("/agenteCargo/editar/{id}")
+    public String mostrarEdicionCargo(@PathVariable Long id, Model model, RedirectAttributes redirectAttributes){
+        Optional<AgenteCargo> cargoOpt = agenteCargoService.findByIdConRelaciones(id);
+        if (cargoOpt.isPresent()){
+            ActualizarAgenteCargoDTO dto = agenteCargoMapper.aDto(cargoOpt.get());
+            model.addAttribute("cargoEdit", dto);
+            return "asignaciones/editar_agente_cargo";
+        } else {
+            redirectAttributes.addFlashAttribute("error", "No se encontró el cargo asignado.");
+            return "redirect:/agentes/";
+        }
+    }
+
+    @PostMapping("/agenteCargo/editar")
+    public String guardarEdicionCargo(@ModelAttribute("cargoEdit") ActualizarAgenteCargoDTO dto, RedirectAttributes redirectAttributes){
+        try {
+
+            Horario horarioAValdiar = new Horario(dto.getHoraInicio(), dto.getHoraFin());
+
+            boolean haySuperposicion = agenteCargoService.existeSuperposicion(dto.getAgenteId(), horarioAValdiar, dto.getId());
+
+            if (haySuperposicion){
+                redirectAttributes.addFlashAttribute("error", "El horario ingresado se superpone con otro cargo activo de este agente.");
+                return "redirect:/agentes/";
+            }
+
+            Optional<AgenteCargo> cargoRealOpt = agenteCargoService.findById(dto.getId());
+            if (cargoRealOpt.isPresent()){
+                AgenteCargo cargoReal = cargoRealOpt.get();
+                if (cargoReal.getHorario() == null){
+                    cargoReal.setHorario(new Horario());
+                }
+
+                agenteCargoMapper.actualizarDesdeDto(dto, cargoReal);
+                agenteCargoService.save(cargoReal);
+
+                redirectAttributes.addFlashAttribute("success", "Cargo actualizado correctamente.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            redirectAttributes.addFlashAttribute("error", "Error al guardar los cambios del cargo.");
+        }
+        return "redirect:/agentes/ver/" + dto.getAgenteId();
     }
 
 }
